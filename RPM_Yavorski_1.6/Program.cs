9580073b486b1b88006385007a77d25e3192f354
Михаил Вагин <mihail.vaginkpc592020@gmail.com>
2023-02-02 00:38:47 +0300

    Добавьте файлы проекта.

---
+++ RPM_Yavorski_1.6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RPM_Yavorski_1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1  13  49");
+        }
+    }
+}
